22f5c7f3cf4842dcdeab22fc82e95fee5b1c7877
Zhang Zhichao <glzhangzhi@outlook.com>
2021-10-15 10:41:16 +0200

    开个新坑，上下班打开的GUI

--- QT/test_1.py
+++ QT/test_1.py
@@ -1,2 +1,41 @@
-# 上述按钮，都继承了QAbstractButton的父类，拥有很多共同的方法
-# 都包含文本和图标
+import sys
+from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtGui import QPainter, QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+class Demo(QWidget):
+    def __init__(self):
+        super(Demo, self).__init__()
+        self.resize(600, 600)
+
+        self.begin_point = QPoint()
+        self.end_point = QPoint()
+
+        self.pix = QPixmap(600, 600)            # 1
+        self.pix.fill(Qt.white)
+
+    def paintEvent(self, QPaintEvent):          # 2
+        painter = QPainter(self.pix)
+        painter.drawLine(self.begin_point, self.end_point)
+        self.begin_point = self.end_point
+        painter2 = QPainter(self)
+        painter2.drawPixmap(0, 0, self.pix)
+
+    def mousePressEvent(self, QMouseEvent):
+        if QMouseEvent.button() == Qt.LeftButton:
+            self.begin_point = QMouseEvent.pos()
+            self.end_point = self.begin_point
+            self.update()
+
+    def mouseMoveEvent(self, QMouseEvent):
+        if QMouseEvent.buttons() == Qt.LeftButton:
+            self.end_point = QMouseEvent.pos()
+            self.update()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    demo = Demo()
+    demo.show()
+    sys.exit(app.exec_())
---
+++ 小脚本/上班辅助工具.py
@@ -0,0 +1,98 @@
+'''
+这是一个用来辅助上班的工具
+
+预计功能：
+
+1. 一键上下班打卡
+2. 显示下班倒计时（可自定义上班时长，显示多余时长）
+
+'''
+
+from time import sleep
+import time
+import sys
+import datetime
+from selenium import webdriver
+from PyQt5.QtWidgets import QApplication, QWidget, QSpinBox, QPushButton, QLabel, QHBoxLayout, QVBoxLayout
+
+def clockin():
+    driver = webdriver.Chrome(r'C:\Users\glzha\Desktop\Git\小脚本\chromedriver.exe')
+    driver.get('http://timeterminal/')
+    name = driver.find_element_by_id("Ident1")
+    name.send_keys('Zhang')
+    sleep(1)
+    password = driver.find_element_by_id("Ident2")
+    password.send_keys('7500478')
+    sleep(1)
+    coming = driver.find_element_by_id('C1')
+    coming.click()
+    print('已打卡登录')
+
+def clockout():
+    driver = webdriver.Chrome(r'C:\Users\glzha\Desktop\Git\小脚本\chromedriver.exe')
+    driver.get('http://timeterminal/')
+    name = driver.find_element_by_id("Ident1")
+    name.send_keys('Zhang')
+    sleep(1)
+    password = driver.find_element_by_id("Ident2")
+    password.send_keys('7500478')
+    sleep(1)
+    going = driver.find_element_by_id('C2')
+    going.click()
+    print('已打卡下班')
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.resize(300, 300)
+
+        self.sp = QSpinBox(self)
+        self.sp.setRange(1, 8)
+        self.sp.setSingleStep(1)
+        self.sp.setValue(8)
+        self.sp.valueChanged.connect(self.changeLbText)
+        
+        self.bt1 = QPushButton('上班', self)
+        # self.bt1.clicked.connect(clockin())
+        
+        hbox1 = QHBoxLayout()
+        hbox1.addStretch(1)
+        hbox1.addWidget(self.sp)
+        hbox1.addWidget(self.bt1)
+        hbox1.addStretch(1)
+
+        self.lb1 = QLabel(self)
+        self.lb1.setText(f"预计下班时间：{(datetime.datetime.now() + datetime.timedelta(hours=9)).strftime('%H:%M')}")
+        self.lb2 = QLabel(self)
+        self.lb2.setText(f'剩余：{540} min')
+
+        self.bt2 = QPushButton('下班', self)
+        # self.bt2.clicked.connect(clockout())
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox1)
+        vbox.addWidget(self.lb1)
+        vbox.addWidget(self.lb2)
+        vbox.addWidget(self.bt2)
+        vbox.addStretch(1)
+
+        self.setLayout(vbox)
+
+        self.show()
+
+    def changeLbText(self, value):
+        if value == 8:
+            value = 9
+        else:
+            value = 4  
+        self.lb2.setText(f'剩余：{value * 60} min')
+        self.lb1.setText(f"预计下班时间：{(datetime.datetime.now() + datetime.timedelta(hours=value)).strftime('%H:%M')}")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Window()
+    sys.exit(app.exec_())
