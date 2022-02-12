3b7942a3d29aa2906ed0f108925ef4899b952f5b
Zhichao Zhang <glzhangzhi@outlook.com>
2022-02-12 10:51:17 +0100

    remove Scripts

--- 小脚本/定时拍照.py
+++
@@ -1,5 +0,0 @@
-import os
-import time
-
-name = './Pictures/' + time.strftime('%m-%d_%H:%M:%S') + '.jpg'
-os.system(f'raspistill -o {name}')
--- 小脚本/重命名selection.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-for file in $(ls *\.txt); do
-	newname=`echo $file | sed "s/"\(.*\)\.txt^"/\1_new\.txt/"`
-	newFile2=`echo $newFile1 | sed "s/\(.*\)\.txt/\1_new\.txt/"`
-	echo $newFile2
-	#mv -i $file $newFile2
-done
